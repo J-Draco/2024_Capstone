08553d2e9bec4c455e10085a6fc76c1899475a93
j4drgn <kimyong0423@naver.com>
2024-11-08 13:07:57 +0900

    메인 페이지 수정, 로고 추가, 네비게이션 수정

--- User/views/main.php
+++ User/views/main.php
@@ -1,4 +1,15 @@
 <?php
-    include  'nav.php';
+    include 'nav.php';
 ?>
-
+<div class="container-fluid p-0 position-relative">
+    <img src="/image/Main.png" class="img-fluid w-100" alt="메인 이미지">
+    <div class="text-center position-absolute bottom-0 start-50 translate-middle-x p-4 text-light">
+        <!-- 실제 표시되는 메인 텍스트 -->
+        <p class="mb-2" id="mainText" style="font-size: 16px;">
+            CodexGuard는 클라우드 기반의 웹 애플리케이션으로, 웹 보안 테스트와 학습을 위한 혁신적인 플랫폼입니다.<br>
+            사용자는 다양한 웹 보안 취약점을 직접 경험하고 실습할 수 있으며, 이를 통해 보안에 대한 깊은 이해를 쌓을 수 있습니다.<br>
+            클라우드 환경을 통해 언제 어디서나 쉽게 접근할 수 있어, 보안 테스트를 보다 효율적으로 수행할 수 있습니다.<br>
+            CodexGuard는 초보자부터 전문가까지 모두에게 유용한 학습 도구를 제공하여, 웹 보안 역량을 강화하는 데 기여합니다.
+        </p>
+    </div>
+</div>
