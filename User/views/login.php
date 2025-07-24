ac2dc2db4eab5ffbaa0797739afa6419719098b0
j4drgn <kimyong0423@naver.com>
2025-07-24 09:56:44 +0900

    refactor: 불필요한 파일 및 디렉토리 제거
    - User/ 디렉토리 제거 (src/app/로 이동됨)
    - public/ 디렉토리 제거 (src/public/로 이동됨)
    - uploads/ 디렉토리 제거 (src/uploads/로 이동됨)

--- User/views/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-#login.php
-    if(isset($_SESSION['username'])){
-        header('Location: index.php?controller=user&action=main');
-    }else{
-        // 로그인하지 않은 경우 로그인 폼 표시
-           include 'loginpage.php';   }
-?>
